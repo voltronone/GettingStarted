b535bf189a74b3549bad08af0e477fa4fbeb9bf7
unknown <darenzan@BMC-BM0Y9W1.adprod.bmc.com>
2014-07-14 22:34:34 -0700

    Added new functionality

--- session_3_basic_grid/BasicRallyGrid/App.js
+++ session_3_basic_grid/BasicRallyGrid/App.js
@@ -10,38 +10,114 @@ Ext.define('CustomApp', {
     launch: function() {
 
       console.log('our first app');     // see console api: https://developers.google.com/chrome-developer-tools/docs/console-api
-      this._loadData();                 // we need to prefix with 'this.' so we call a method found at the app level.
+	
+		this.pulldownContainer = Ext.create('Ext.container.Container', {
+			id : 'pulldownContainer',
+			layout: {
+				type: 'hbox',
+				align: 'stretch'
+			},
+		});
+	  
+	  this.add(this.pulldownContainer);
+	  this._loadIterations();
     },
 
+	_loadIterations: function() {
+		this.iterations = Ext.create('Rally.ui.combobox.IterationComboBox', {
+		fieldLabel : 'Iteration',
+		labelAlign : 'right',
+		width : 500,
+		listeners : {
+			ready: function(combobox) {
+				console.log('ready!', combobox);
+				//this._loadData();
+				this._loadOwners();
+			},
+			select: function(combobox, records) {
+				this._loadData();
+			},
+			scope: this
+		},
+		});
+		this.pulldownContainer.add(this.iterations);
+	},
+	
+	_loadOwners: function() {
+		this.owners = Ext.create('Rally.ui.combobox.FieldValueComboBox', {
+		model : 'TestSet',
+		field : 'Owner',
+		fieldLabel : 'Owner',
+		labelAlign : 'right',
+		listeners : {
+			ready: function(combobox){
+				this._loadData();
+			},
+			select: function(combobox, records) {
+				this._loadData();
+			},
+			scope:this
+		},
+		});
+		this.pulldownContainer.add(this.owners);
+	},
+	
     // Get data from Rally
     _loadData: function() {
 
-      var myStore = Ext.create('Rally.data.wsapi.Store', {
-          model: 'User Story',
+	var selectedIteration = this.iterations.getRecord().get('_ref');
+	var selectedOwner = this.owners.getRecord().get('value');
+	console.log('chosen one: ', selectedOwner);
+	
+	var myFilters = [
+		  {
+			property: 'Iteration',
+			operation: '=',
+			value: selectedIteration
+		  }, 
+		  {
+			property: 'Owner',
+			operation: '=',
+			value: selectedOwner
+		  }
+		];
+
+	if (this.myStore) {
+		
+		this.myStore.setFilter(myFilters);
+		this.myStore.load();
+		
+	} else {
+      this.myStore = Ext.create('Rally.data.wsapi.Store', {
+          model: 'TestSet',
           autoLoad: true,                         // <----- Don't forget to set this to true! heh
+		  filters: myFilters,
           listeners: {
               load: function(myStore, myData, success) {
                   console.log('got data!', myStore, myData);
-                  this._loadGrid(myStore);      // if we did NOT pass scope:this below, this line would be incorrectly trying to call _createGrid() on the store which does not exist.
+                  if (!this.myGrid) {
+				  this._loadGrid(myStore); // if we did NOT pass scope:this below, this line would be incorrectly trying to call _createGrid() on the store which does not exist.
+				  }
               },
               scope: this                         // This tells the wsapi data store to forward pass along the app-level context into ALL listener functions
           },
-          fetch: ['FormattedID', 'Name', 'ScheduleState']   // Look in the WSAPI docs online to see all fields available!
+          fetch: ['FormattedID', 'Name','Iteration', 'Owner']   // Look in the WSAPI docs online to see all fields available!
         });
+	}
 
     },
 
     // Create and Show a Grid of given stories
     _loadGrid: function(myStoryStore) {
 
-      var myGrid = Ext.create('Rally.ui.grid.Grid', {
+      this.myGrid = Ext.create('Rally.ui.grid.Grid', {
         store: myStoryStore,
         columnCfgs: [         // Columns to display; must be the same names specified in the fetch: above in the wsapi data store
-          'FormattedID', 'Name', 'ScheduleState'
+          'FormattedID', 'Name', 'Iteration', 'Owner'
         ]
       });
 
-      this.add(myGrid);       // add the grid Component to the app-level Container (by doing this.add, it uses the app container)
+      this.add(this.myGrid);       // add the grid Component to the app-level Container (by doing this.add, it uses the app container)
 
       console.log('what is this?', this);
 
